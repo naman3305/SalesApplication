1102095a357e6fb5201ff97892f6234872ae7a26
naman3305 <59595534+naman3305@users.noreply.github.com>
2020-11-18 10:35:01 +0530

    Update DBUtil.java

--- src/com/wipro/sales/util/DBUtil.java
+++ src/com/wipro/sales/util/DBUtil.java
@@ -3,7 +3,7 @@ import java.sql.*;
 public class DBUtil {
 	public static Connection getDBConnection() throws Exception{
 		Connection con = null;
-			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/SalesSystem?autoReconnect=true&useSSL=false","root","password");
+			 con =DriverManager.getConnection("jdbc:mysql://localhost:3306/SalesApplication","root"," "); 
 		return con;
 	}
 }
